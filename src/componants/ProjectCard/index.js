9772e1034002f0d6f3b37b0e4917d2a6736f363e
freyacooper <138793044+freyacooper@users.noreply.github.com>
2024-04-06 10:17:15 +0200

    animation du scroll et mise en page des cartes

--- src/componants/ProjectCard/index.js
+++ src/componants/ProjectCard/index.js
@@ -1,6 +1,6 @@
 import './index.scss'
 
-function ProjectCard({ title, tagline, cover, color, url }) {
+function ProjectCard({ title, tagline, cover, color }) {
     return(
         <div className="card" style={{backgroundColor: color}}>
             <img alt="A screenshot of the website" src={cover}/>
@@ -10,12 +10,12 @@ function ProjectCard({ title, tagline, cover, color, url }) {
                { title === "Bivo" ? (
                 <button className='disabled' disabled>Coming soon !</button>
                ) : (
-            //     <Link to={`/projet/${url}`}>
-            //         <button>See case study</button>
-            //    </Link>
-            <button>See case study</button>
-               )
-               }
+                <div className='button-container'>
+                    <button>See code</button>
+                    <button>Go to website</button>
+                </div>
+                
+               )}
             </div>
         </div>
     )
