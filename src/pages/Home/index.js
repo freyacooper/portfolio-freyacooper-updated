827d3d63bb2303a306ec8132379cf503c7602565
freyacooper <138793044+freyacooper@users.noreply.github.com>
2024-05-06 21:09:46 +0200

    OMF page projet besoin des images du code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -98,7 +98,7 @@ function Home() {
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <path fill="#211349" d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/>
                             </svg>
-                            email@gmail.com
+                            contact@freya-cooper.com
                             <span>
                                 <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 233.67 233.7">
                                 <g id="Layer_1-2" data-name="Layer 1">
@@ -109,7 +109,7 @@ function Home() {
                             </span>
                         </button> 
                         </a> 
-                        <a href='mailto:email@gmail.com'>
+                        <a href='mailto:contact@freya-cooper.com'>
                         <button>
                         <svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 69.7 69.7">
                             <g id="Layer_1-2" data-name="Layer 1">
--- src/pages/OMF/index.js
+++ src/pages/OMF/index.js
@@ -2,6 +2,13 @@ import omfbanner from '../../Photos/omfbanner.png'
 import homepage from '../../Photos/screens/OMFDeskandMob.png'
 import './index.scss'
 import { useEffect } from 'react';
+import restaurant from '../../Photos/elements/omfrestaurant.jpg'
+import tick from '../../Photos/elements/tick.svg'
+import omfmob from '../../Photos/elements/omfmob.png'
+import omfdesk from '../../Photos/elements/omfdesk.png'
+import arrow from '../../Photos/elements/arrow_2.png'
+import menuanim from '../../Photos/elements/menuanim2.gif'
+import homeanim from '../../Photos/elements/homeanim.gif'
 
 function ProjOMF() {
     useEffect(() => {
@@ -28,12 +35,148 @@ function ProjOMF() {
                         <img src={homepage} alt=''/>
                     </div>
                     <div className='text'>
-                        <p>Jennings Chimney Sweeping is a dedicated one-man operation, specializing in chimney sweeping services while also prioritizing education and safety through their informative website and blog space.</p>
-                        <p>My role in this project was to redesign the entire website, giving it a modern and inviting aesthetic while ensuring user-friendliness and accessibility to the wealth of information available. Additionally, I created a separate page specifically dedicated to the extensive articles and educational content, aptly named the "blog space." Alongside the redesign, I implemented adjustments to optimize the site for mobile devices, enhancing its reach and usability across various platforms.</p>
+                        <p>Welcome to the world of Oh My Food—a conceptual venture imagined to revolutionize the dining experience. As well as classic reservation systems, Oh My Food's website allows users to select items from the menus and place orders from a distance, ensuring a swift dining experience and lowering restaurant wait-times.</p>
+                        <p>In this project, I was tasked with bringing Oh My Food's innovative vision to life through web development. With an emphasis on the use of Sass to organize the styling and leveraging CSS animations, the aim was to create a dynamic and engaging interface that captivates users from the moment they land on the page. Through a mobile-first approach, the goal was to craft an experience that seamlessly integrates advanced styling techniques, enhancing user interaction and elevating Oh My Food's online presence.</p>
                     </div>
                 </div>
                 <div>
-                    <h2>Brand Identity</h2>
+                    <h2>Mobile-first approach</h2>
+                    <p>In crafting the Oh My Food website, a mobile-first approach was essentiel for ensuring a user experience tailored to the needs of our target audience. With busy professionals in mind, our primary users are individuals seeking to enjoy a dine-in restaurant experience during their lunch breaks. As our users are often on the go, accessing the website from their smartphones is the norm. Therefore, adopting a mobile-first strategy was a natural choice, allowing us to prioritize mobile usability and optimize the interface for smaller screens. By designing with mobile users in mind, we ensure that Oh My Food remains a convenient and efficient solution for our busy clientele.</p>
+                    <div className='mobile-first'>
+                        <img className='mobfirst-screen' src={omfmob} alt='mobile version of website'/>
+                        <img className='arr' src={arrow} alt='arrow pointing to the right'/>
+                        <img className='mobfirst-screen' src={omfdesk} alt='desktop version of website'/>
+                    </div>
+                </div>
+                <div className='section-div'>
+                    <h2>Advantages of Sass</h2>
+                    <p>In this project, I was introduced to the power of Sass, a CSS preprocessor that revolutionizes the way stylesheets are written and managed. Sass extends the capabilities of CSS by introducing features like variables, mixins, nesting, and more. With Sass, stylesheets can be broken down into smaller, modular components, facilitating better organization and enhancing code readability. Additionally, Sass's variable feature allows for the creation of reusable values, making it simpler to maintain consistency across the project and facilitating future updates.</p>
+                </div>
+                <div>
+                    <h2>Animations</h2>
+                   
+                        <p>This project placed a significant emphasis on incorporating animations to enhance the user experience. Animations play a crucial role in capturing user attention, guiding interactions, and adding a layer of engagement to the website. By incorporating animations strategically, we aimed to create a dynamic and memorable browsing experience for visitors.</p>
+                    <div className='p1'>     
+                        <img className='gif' src={menuanim} alt='gif showing menu animation'/>
+                        <img className='gif' src={homeanim} alt='gif showing homepage animation'/>
+                    </div>
+                        <p>The animations in this project were built entirely with CSS, meaning they are lightweight and do not require additional JavaScript libraries or plugins, resulting in faster load times and smoother performance. This approach also promotes cleaner code and easier maintenance, as animations can be seamlessly integrated into the existing stylesheet. </p>
+                    
+                    <div className='menu-div'>
+    <div class="rest-image">
+        <img src={restaurant} alt="Une assiette de nourriture sur une table."/>
+    </div>
+    <div class="restaurant">
+        <div class="restaurant__title">
+            <h2>À la française</h2>
+            <div class="hearts-wrapper">
+                <svg class="liked" width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M19.865 1.43593C17.5103 -0.753131 14.0083 -0.359381 11.847 2.07343L11.0005 3.02499L10.154 2.07343C7.99701 -0.359381 4.49076 -0.753131 2.13607 1.43593C-0.562363 3.94843 -0.704161 8.45781 1.71068 11.1812L10.0251 20.5469C10.5622 21.1516 11.4345 21.1516 11.9716 20.5469L20.2861 11.1812C22.7052 8.45781 22.5634 3.94843 19.865 1.43593Z" fill="url(#paint0_linear_90763_527)"/>
+                    <defs>
+                    <linearGradient id="paint0_linear_90763_527" x1="11.0002" y1="-5.4601" x2="3.43443" y2="27.9923" gradientUnits="userSpaceOnUse">
+                    <stop stop-color="#9356DC" offset="0"/>
+                    <stop offset="1" stop-color="#FF79DA"/>
+                    </linearGradient>
+                    </defs>
+                </svg>
+                <svg width="22" height="21" viewBox="0 0 22 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <mask id="path-1-inside-1_90763_518" fill="white">
+                    <path d="M19.865 1.43593C17.5103 -0.753131 14.0083 -0.359381 11.847 2.07343L11.0005 3.02499L10.154 2.07343C7.99701 -0.359381 4.49076 -0.753131 2.13607 1.43593C-0.562363 3.94843 -0.704161 8.45781 1.71068 11.1812L10.0251 20.5469C10.5622 21.1516 11.4345 21.1516 11.9716 20.5469L20.2861 11.1812C22.7052 8.45781 22.5634 3.94843 19.865 1.43593Z"/>
+                    </mask>
+                    <path d="M19.865 1.43593C17.5103 -0.753131 14.0083 -0.359381 11.847 2.07343L11.0005 3.02499L10.154 2.07343C7.99701 -0.359381 4.49076 -0.753131 2.13607 1.43593C-0.562363 3.94843 -0.704161 8.45781 1.71068 11.1812L10.0251 20.5469C10.5622 21.1516 11.4345 21.1516 11.9716 20.5469L20.2861 11.1812C22.7052 8.45781 22.5634 3.94843 19.865 1.43593Z" stroke="#101010" stroke-width="2" mask="url(#path-1-inside-1_90763_518)"/>
+                </svg>
+            </div>
+        </div>
+        <div class="menu">
+            <div class="menu__course">
+                <h3>Entrées</h3>    
+            </div>
+            <div class="menu__group">
+                <div class="menu-item menu-item--1">
+                    <div class="menu-item__content">
+                        <h4>Tartare de poulpe acidulé</h4>
+                        <p>Aux zestes d'orange</p> 
+                    </div>
+                    <h5>25€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+                <div class="menu-item menu-item--2">
+                    <div class="menu-item__content">
+                        <h4>Velouté de légumes d'antan</h4>
+                        <p>Carotte, panais, topinambour</p> 
+                    </div>
+                    <h5>35€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+            </div>
+            <div class="menu__course">
+                <h3>Plats</h3>    
+            </div>
+            <div class="menu__group">
+                <div class="menu-item menu-item--1">
+                    <div class="menu-item__content">
+                        <h4>Coquilles Saint-Jacques</h4>
+                        <p>Accompagnées d'une purée de panais</p> 
+                    </div>
+                    <h5>40€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+                <div class="menu-item menu-item--2">
+                    <div class="menu-item__content">
+                        <h4>Magret de canard</h4>
+                        <p>Et parmentier de pommes de terre</p> 
+                    </div>
+                    <h5>35€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+                <div class="menu-item menu-item--3">
+                    <div class="menu-item__content">
+                        <h4>Pigeonneau d’Ille-et-Vilaine</h4>
+                        <p>Sur son lit de gnocchis aux légumes</p> 
+                    </div>
+                    <h5>44€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+            </div>
+            <div class="menu__course">
+                <h3>Desserts</h3>    
+            </div>
+            <div class="menu__group">
+                <div class="menu-item menu-item--1">
+                    <div class="menu-item__content">
+                        <h4>Fondant au chocolat</h4>
+                        <p>Revisitée</p> 
+                    </div>
+                    <h5>22€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+                <div class="menu-item menu-item--2">
+                    <div class="menu-item__content">
+                        <h4>Millefeuille croustillant</h4>
+                        <p>Myrtilles et pâte d’amande</p> 
+                    </div>
+                    <h5>23€</h5>
+                    <div class="menu-item__check">
+                        <img src={tick} alt='tick'/>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="button">Commander</div>
+    </div>
+                    </div>
                 </div>
             </div>
         </div>
