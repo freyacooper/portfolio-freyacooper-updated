555e46498247237447c0a36d3728b711a5412b2c
freyacooper <138793044+freyacooper@users.noreply.github.com>
2024-05-23 15:54:50 +0200

    testing decoding images

--- public/preload.js
+++ public/preload.js
@@ -10,5 +10,10 @@ window.addEventListener('load', () => {
     imagesToPreload.forEach(src => {
       const img = new Image();
       img.src = src;
+      img.decode().then(() => {
+        console.log(`${src} is decoded`);
+      }).catch((err) => {
+        console.error(`Decoding ${src} failed: `, err);
+      });
     });
   });
